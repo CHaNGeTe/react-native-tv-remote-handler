c4c9939a300512d3748f0216c8a85dc99d95ec7e
aurimasmi <aurimasmickys@gmail.com>
2021-04-22 16:42:12 +0300

    default ios handler

--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -1,16 +1,54 @@
 import { useEffect } from 'react';
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import {
+  // NativeModules,
+  // NativeEventEmitter,
+  TVEventHandler,
+} from 'react-native';
 
-const useTVRemoteHandler = (callback) => {
+const useTVRemoteHandler = (callback: any, filterEvents = ['focus']) => {
+  let tvEventHandler: any = null;
+
+  const enableEventHandler = () => {
+    if (!tvEventHandler) {
+      tvEventHandler = new TVEventHandler();
+      tvEventHandler.enable(null, (_: any, evt: any) => {
+        if (filterEvents.length) {
+          if (filterEvents.includes(evt.eventType)) {
+            callback(evt);
+          }
+        } else {
+          callback(evt);
+        }
+      });
+    }
+  };
+
+  const disableEventHandler = () => {
+    if (tvEventHandler) {
+      tvEventHandler.disable();
+      tvEventHandler = null;
+    }
+  };
+
   useEffect(() => {
-    const { TvRemoteHandler } = NativeModules;
-    const event = new NativeEventEmitter(TvRemoteHandler);
-    event.addListener('onKeyDown', (res) => {
-      console.log('MyEvent', res);
-    });
+    enableEventHandler();
+
+    return () => disableEventHandler();
   });
 
   return {};
 };
+
+// const useTVRemoteHandler = (callback) => {
+//   useEffect(() => {
+//     const { TvRemoteHandler } = NativeModules;
+//     const event = new NativeEventEmitter(TvRemoteHandler);
+//     event.addListener('onKeyDown', (res) => {
+//       console.log('MyEvent', res);
+//     });
+//   });
+
+//   return {};
+// };
 
 export { useTVRemoteHandler };
